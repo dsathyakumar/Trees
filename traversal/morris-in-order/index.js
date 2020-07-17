592ceb01bd4fb1e3d0d4e1d0d298fdde430466ef
dsathyakumar <gautham.daamodhar@gmail.com>
2020-07-17 14:29:09 -0600

    +Morris in-order

---
+++ traversal/morris-in-order/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+exports.morrisInOrder = root => {
+    const result = [];
+
+    if (!root) {
+        return result;
+    }
+
+    let currentNode = root;
+    let inOrderPredecessor = null;
+
+    while (currentNode) {
+        // Check if a LEFT subTree exists, process it
+        if (currentNode.left) {
+            // start by setting inOrder predecessor as LEFT subTree root.
+            // if this node, did not have a RIGHT, then this would end up
+            // becoming the inOrderPredecessor
+            inOrderPredecessor = currentNode.left;
+
+            // as long as inOrderPredecessor exists, there are 3 cases
+            while (inOrderPredecessor) {
+                // 1) When it has a RIGHT
+                if (inOrderPredecessor.right) {
+                    // Case 1A) RIGHT is not the currentNode
+                    // current = inOrderPredecessor.right
+                    // continue inner loop
+                    if (inOrderPredecessor.right !== currentNode) {
+                        inOrderPredecessor = inOrderPredecessor.right;
+                        continue;
+                    } else {
+                        // Case 1B) RIGHT is the currentNode
+                        // inOrderPredecessor.right = null
+                        // process the currentNode
+                        // set currentNode = currentNode.right
+                        // break inner loop
+                        inOrderPredecessor.right = null;
+                        result.push(currentNode.val);
+                        currentNode = currentNode.right;
+                        break;
+                    }
+                } else {
+                    // 2) When it does not have a RIGHT
+                    // inOrderPredecessor.right = currentNode
+                    // currentNode = currentNode.left
+                    // break inner loop
+                    inOrderPredecessor.right = currentNode;
+                    currentNode = currentNode.left;
+                    break;
+                }
+            }
+
+            continue;
+        }
+
+        // if not LEFT subTree, process currentNode.
+        result.push(currentNode.val);
+
+        // if RIGHT subTree exists, process it & outer loop continues
+        if (currentNode.right) {
+            currentNode = currentNode.right;
+        }
+    }
+
+    return result;
+};
---
+++ traversal/morris-in-order/playground/playground.js
@@ -0,0 +1,33 @@
+const {
+    arrayToBinaryTree
+} = require('../../../construct/arrayToBinaryTree');
+
+const {
+    morrisInOrder
+} = require('../index');
+
+// ----------- Binary Tree construction from Array
+/** The Given Binary Tree is:
+ *  ----------------------------
+ *            A
+ *      _____/ \________
+ *     /                \
+ *     B                C
+ *    /              ___/ \___
+ *    G             /         \
+ *                D          E
+ *                       ___/
+ *                      /
+ *                      F
+ */
+const T_1 = arrayToBinaryTree([
+    'A',
+    'B', 'C',
+    'G', null, 'D', 'E',
+    null, null, null, null, null, null, 'F', null
+]);
+console.log(JSON.stringify(T_1, null, 3));
+
+// ----------- in-order traversal
+//should print [G, B, A, D, C, F, E]
+console.log(morrisInOrder(T_1));
