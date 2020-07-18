bda753cdac83e093f3ca04692f6ce65567de6d1b
dsathyakumar <gautham.daamodhar@gmail.com>
2020-07-18 16:19:43 -0600

    +fix top view

---
+++ views/top-view/playground/playground.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const {
+    arrayToBinaryTree
+} = require('../../../construct/arrayToBinaryTree');
+
+const {
+    topView
+} = require('../top-view');
+
+const T_1 = arrayToBinaryTree([
+    1,
+    2, 3,
+    4, 5, 6, 7
+]);
+
+// should print [4, 2, 1, 3, 7]
+console.log(topView(T_1));
+
+
+const T_3 = arrayToBinaryTree([
+    1,
+    2, 3,
+    null, 4, null, null,
+    null, null, null, 5, null, null, null, null,
+    null, null, null, null, null, null, null, 6, null, null, null, null, null, null, null, null
+]);
+
+// should print [2, 1, 3, 6]
+console.log(topView(T_3));
--- views/top-view/top-view.js
+++ views/top-view/top-view.js
@@ -8,7 +8,7 @@
  * @returns {Array} result
  */
 exports.topView = root => {
-    const result = [];
+    const result = {};
 
     if (!root) {
         return result;
@@ -32,28 +32,30 @@ exports.topView = root => {
         // get the node's hd from map
         hd = nodeToDistanceMap.get(deqNode);
 
+        if (!result[hd]) {
+            result[hd] = [];
+        }
+        result[hd].push(deqNode.val);
+
         // if LEFT, enQ in rear, updated HD and store in map
         if (deqNode.left) {
-            hd = hd - 1;
             q.push(deqNode.left);
-            nodeToDistanceMap.set(deqNode.left, hd);
+            nodeToDistanceMap.set(deqNode.left, hd - 1);
         }
 
         // if RIGHT, enQ in rear, updated HD and store in map
         if (deqNode.right) {
-            hd = hd + 1;
             q.push(deqNode.right);
-            nodeToDistanceMap.set(deqNode.right, hd);
+            nodeToDistanceMap.set(deqNode.right, hd + 1);
         }
 
         // delete the entry from the map.
         nodeToDistanceMap.delete(deqNode);
     }
 
-    // pick the 1st value from every entry
-    for (let count = 0; count < result.length; count++) {
-        result[count] = result[count][0]
-    }
+    Object.keys(result).forEach(key => {
+        result[key] = result[key][0];
+    })
 
     return result;
 };
