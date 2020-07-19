1f07cd300b643e285c08e8c3d9fc8af25d116465
dsathyakumar <gautham.daamodhar@gmail.com>
2020-07-19 14:22:50 -0600

    chore: more samples

--- traversal/morris-post-order/playground/playground.js
+++ traversal/morris-post-order/playground/playground.js
@@ -15,4 +15,17 @@ const T_1 = arrayToBinaryTree([
 console.log(JSON.stringify(T_1, null, 3));
 
 // ----------- morris post order traversal
+// [F, C, E, D, B, X, A]
 console.log(morrisPostOrder(T_1));
+
+const T_2 = arrayToBinaryTree([
+    'A',
+    'B', 'C',
+    'G', null, 'D', 'E',
+    null, null, null, null, null, null, 'F', null
+]);
+console.log(JSON.stringify(T_2, null, 3));
+
+// ----------- post-order traversal
+// should print [G, B, D, F, E, C, A]
+console.log(morrisPostOrder(T_2));
