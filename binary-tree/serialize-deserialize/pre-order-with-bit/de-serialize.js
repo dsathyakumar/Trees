f7bc390e1f46f6b0c3b18e587a5871da8f4dfb7f
dsathyakumar <gautham.daamodhar@gmail.com>
2020-07-27 21:54:52 -0600

    chore: + docs

--- binary-tree/serialize-deserialize/pre-order-with-bit/de-serialize.js
+++ binary-tree/serialize-deserialize/pre-order-with-bit/de-serialize.js
@@ -10,8 +10,12 @@ const {
  * Fr eg) A' B' / D C' E' G / F' H I
  * 
  * The key is:
- * - Left child becomes a LEAF to current Parent.
+ * - When Left child becomes a LEAF to current Parent.
+ *      => Do not change the parent Node, but the next processable Node
+ *          would not be a LEFT, but a RIGHT instead.
  * - Right Child becomes a LEAF to current Parent.
+ *      => Parent Node has to be changed, but the next processable Node
+ *          would continue to be the Right Node of the parent.
  * 
  * @param {String} data
  * @returns {BinaryTreeNode} root
@@ -39,6 +43,7 @@ exports.deserialize = data => {
             current = current.substring(0, current.length - 1);
         }
 
+        // handle internal Node types
         if (isInternal) {
             if (current !== '/') {
                 // create the Node for the current element
@@ -64,6 +69,7 @@ exports.deserialize = data => {
                 }
             }
         } else {
+            // handle LEAF Node types.
             if (parentNode) {
                 if (isLeft) {
                     isLeft = false;
@@ -71,19 +77,26 @@ exports.deserialize = data => {
                     if (current !== '/') {
                         parentNode.left = new BinaryTreeNode(current);
                     }
+                    // here there is no need to modify the parent as itys RIGHT
+                    // must be processed next.
                 } else {
                     if (current !== '/') {
                         parentNode.right = new BinaryTreeNode(current);
                     }
-
+                    // since this a RIGHT, the current Parent is completely
+                    // processed. Time to process the RIGHT of a previous ancestor.
                     parentNode = stack.shift();
                 }
             } else {
+                // if no parent, then current is the root
                 root = new BinaryTreeNode(current);
             }
         }
 
+        // get the next serialized element
         current = data.shift();
+
+        // reset internal Node.
         isInternal = false;
     }
 
